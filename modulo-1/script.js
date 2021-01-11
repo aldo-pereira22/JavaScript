38948362f1aa08394ae4812a702087ff942a0e36
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-01-11 19:23:51 -0300

    Modulo-1
    Modulo 1 finalizado.

---
+++ modulo-1/script.js
@@ -0,0 +1,55 @@
+
+//JAVASCRIPT
+
+//FORMAS DE MOSTRAR MENSAGENS
+// alert('Opa,Estou funcionando')
+// document.write('Testando 1 2 3...')
+// document.getElementById('titulo').innerHTML = 'Opa, Tudo bem ?'
+// window.alert('Mensagem de exemplo')
+// console.log('A mensagem que eu inserir aqui aparecerá no console')
+
+//DECLARAÇÃO DE VARIÀVEIS
+// var idade = 90;
+// var nome = ' Aldo'
+
+// alert(idade);
+// alert(nome)
+// var x = 10;
+// var y = 15;
+// var total = x + y ;
+// alert(total)
+
+
+// var nome = 'Aldo';
+// var sobreNome = 'Pereira';
+
+// var nomeCompleto = nome  + sobreNome;
+
+// alert(nomeCompleto)
+
+
+// CONDICIONAIS
+
+// var hora = 15;
+
+// if( hora < 12) {
+
+//     console.log("Bom Dia");
+ 
+// } else {
+//     console.log("Não é de manhã");
+// }
+
+
+// OUTRAS DEFINIÇÕES DE VARIÀVEIS
+
+var nome =  'Aldo';
+let nome = 'Aldo';
+const nome = 'Aldo';
+
+// Var = HOISTING Esse tipo de declaração envia a variável
+// para o escopo geral da página 
+
+// LET = A variável so fica disponivel no escopo da função
+
+// CONST =  valor constante, o valor não pode receber um novo valor
