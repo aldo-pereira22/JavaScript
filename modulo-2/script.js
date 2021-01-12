fcd31830b9042c29813ed5319e00e72823cf43dc
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-01-12 19:38:25 -0300

    Modulo-3

--- modulo-2/script.js
+++ modulo-2/script.js
@@ -64,3 +64,124 @@ function a( elemento ){
         document.getElementById('telefone').style.display ="block"
         elemento.style.display = 'none'
 }
+
+
+// ARRAYS
+
+// let carros = ['palio', 'Uno', 'Corolla', 'Ferrari', function(){
+//     console.log(carros)
+// }]
+
+// console.log(carros[4]())
+
+// let ingredientes = [
+//     ['uva', 'pera', 'Abacate'],
+//     ['Arroz', 'Macarrão']
+// ]
+
+// alert( ingredientes[1][1]);
+// alert(ingredientes[0][2])
+
+
+//OBJETOS
+
+// let carro = {
+//     nome: 'Fiat',
+//     Modelo: ' Uno',
+//     peso: '800 kg',
+//     ligar: function(){
+//         alert('Vrum Vrumm')
+//     },
+
+//     acelerar: function(){
+//         alert('zummmmm')
+//     }
+// }
+// carro.ligar()
+// carro.acelerar()
+
+//Switch 
+
+let dia  = 2;
+let diaNome 
+// switch(dia){
+//     case 1: 
+//         diaNome = 'Segunda-Feira';
+//         break;
+    
+//     case 2: 
+//         diaNome = 'Terça-Feira';
+//         break;
+
+//     case 3: 
+//         diaNome = 'Quarta-Feira';
+//         break;
+
+//     case 4: 
+//         diaNome = 'Quinta-Feira';
+//         break;
+
+//     case 5:
+//         diaNome = 'Sexta-Feira'
+//         break;
+
+//     case 6: 
+//         diaNome =  'Sabado'
+//         break;
+
+//     case 7:
+//         diaNome = 'Domingo'
+//         break;
+
+    
+// }
+
+
+// switch (dia) {
+//     case 6:
+//         diaNome = 'Final de Semana'
+//         break;
+
+//     case 7: 
+//         diaNome = ' Final de Semana'
+//     default:
+//         diaNome = 'Dia de semana! Vai trabalhar'
+//         break;
+// }
+// document.getElementById('dia').innerHTML = 'Hoje é: '+diaNome
+
+
+
+// ITERAÇÃO DO FOR NORMAL
+// let texto = ''
+// for( let i = 0; i <= 10 ; i++ ){
+//     texto =  texto  + i + '<br>';
+// }
+
+
+// ITERAÇÃO DO FOR COM ARRAY 
+
+// let carros = ['Ferrai', 'Fusca', 'Palio', 'Corolla', 'Bugato', 'HB20-S', 'JETA'];
+
+
+// let html = '<ul>';
+
+// for( let i in carros ){
+//     html += '<li>'+ carros[i] +'</li>'
+// }
+
+// html += '</ul>';
+
+
+
+// let html = ''
+// let c = 0 ;
+
+// while ( c < 10) {
+//     html += " <h3> Numero </h3>: " + c + "<br>"
+//     c++;
+// }
+
+
+
+document.getElementById('demo').innerHTML = html;
---
+++ modulo-3/script.js
@@ -0,0 +1,17 @@
+// let nome = 'Aldo Pereira pinto'
+
+// let resultado = ''
+
+// if (nome.indexOf('Ma') > -1 ){
+
+//     resultado = 'Achou!!! '
+// }else {
+//     resultado = 'Não achou'
+// }
+// alert(resultado)
+
+let nome = 'Aldo Pereira Pinto'
+
+let resultado = nome.slice(0,10)
+
+alert(resultado)
