ada2f55e0f0ec7f7b0f298acdb38a0a6b46ca983
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-02-04 18:18:05 -0300

    Projeto pizza finalzado

--- modulo-4/pizza_base/script.js
+++ modulo-4/pizza_base/script.js
@@ -134,7 +134,19 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     updateCart();
 })
 
+c('.menu-openner').addEventListener('click', ()=> {
+    if(cart.length > 0 ){
+        c('aside').style.left = '0';
+    }
+})
+
+c('.menu-closer').addEventListener('click', ()=> {
+    c('aside').style.left = '100vw';
+})
+
 function updateCart(){
+
+    c('.menu-openner span').innerHTML = cart.length;
 
     if(cart.length > 0 ){
         c('aside').classList.add('show');
@@ -203,7 +215,8 @@ function updateCart(){
         c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
 
     }else {
-        c('aside').classList.remove('show')
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw'
     }
 }
 
